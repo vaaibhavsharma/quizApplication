2bc25fb1d80c0225f3394895e0a7f84ef6b95e09
vaibhavwin <asvaibhavsharma@gmail.com>
2022-10-06 02:59:01 +0530

    few changes fro deploying it on aws rds⭐

--- simpleQuiz2/settings.py
+++ simpleQuiz2/settings.py
@@ -75,12 +75,26 @@ WSGI_APPLICATION = 'simpleQuiz2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if env('environment') == 'dev':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': env('DB_NAME'),
+            'HOST': env('HOST'),
+            'USER': 'admin',
+            'PASSWORD': env('PASSWORD'),
+            'PORT': '3306'
+        }
+    }
+
+
 
 
 
